5f12abb57c4395f78ee3a7655c7ff83681cc7d04
abitshrestha <abitshrestha2079@gmail.com>
2022-08-09 21:36:47 -0700

    first PHP

---
+++ prog.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+    <body>
+        <h1>This is the first PHP</h1>
+        <?php 
+        $name="ABIT";
+        echo "Hello World,I am $name";
+        ?>
+    </body>
+</html>
