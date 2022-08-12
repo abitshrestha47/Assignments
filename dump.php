289fd85f3d672cb3856fcda37ebe7f920fdd32b6
abitshrestha <abitshrestha2079@gmail.com>
2022-08-11 18:55:14 -0700

    dump function

---
+++ dump.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+		<head>
+    		<title>Dump Function</title>
+        </head>
+        <body>
+        	<?php
+            	$var=1;
+            	$str="Ram";
+           		$bool=true;
+            	var_dump ($var,$str);  
+            	var_dump ($bool);
+        	?>
+    	</body>
+</html>
