14a2b1148af1ac2d1be7e904248dea0fdf7c3dc4
abitshrestha <abitshrestha2079@gmail.com>
2022-08-21 22:51:10 -0700

    strlen && strpos

---
+++ stringfunction.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>String functions</title>
+    </head>
+<body>
+
+<?php
+	$anonym=function(){
+    	echo "<br>";
+    };
+    $str="learning php in fun";
+    echo strlen($str);
+    
+    $anonym();
+    $str1="learning php day by day";
+    echo strpos($str1,"php")
+?>
+ 
+
+</body>
+</html>
