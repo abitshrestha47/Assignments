9aa5d9b0d3bb7c85cb2c3620d83c4c5a0b00fe3c
abitshrestha <abitshrestha2079@gmail.com>
2022-08-25 10:00:19 -0700

    associative array & print

---
+++ arrayprint.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+	$arr=array("ID"=>"33","city"=>"ktm");
+    echo $arr["ID"];
+    echo "<br>";
+    print_r($arr);
+    echo "<br>";
+    var_dump($arr);
+    
+?>
+
+</body>
+</html>
